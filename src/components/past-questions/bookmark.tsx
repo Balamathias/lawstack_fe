36216b92c8ae899e3dc86c7a9180e650c86c8be1
Balamathias <balamathias05@gmail.com>
2025-02-23 15:13:43 +0100

    Use optimistic updates implemented

--- src/components/past-questions/bookmark.tsx
+++ src/components/past-questions/bookmark.tsx
@@ -1,10 +1,10 @@
 'use client'
 
 import { cn } from '@/lib/utils';
-import { useCreateBookmark, useDeleteBookmark } from '@/services/client/bookmarks';
-import { Loader2, LucideBookmark } from 'lucide-react'
+import { useCreateBookmark } from '@/services/client/bookmarks';
+import { LucideBookmark } from 'lucide-react'
 import { useRouter } from 'next/navigation';
-import React from 'react'
+import React, { useOptimistic, startTransition } from 'react'
 import { toast } from 'sonner';
 
 interface Props {
@@ -14,43 +14,43 @@ interface Props {
 
 const Bookmark = ({ isbookmarked, question_id }: Props) => {
   const { mutate: bookmarkQuestion, isPending } = useCreateBookmark()
-  const { mutate: unbookmarkQuestion, isPending: isUnbookmarkPending } = useDeleteBookmark()
   const router = useRouter()
+  const [optimisticState, setOptimisticState] = useOptimistic(isbookmarked)
 
   const handleBookmarkQuestion = () => {
+    startTransition(() => {
+      setOptimisticState(!optimisticState)
+    })
+
     bookmarkQuestion({ past_question_id: question_id }, {
         onSuccess: (data) => {
-            
             if (data?.error) {
+                setOptimisticState(isbookmarked) // Revert state on error
                 return toast?.error(data.message)
             }
 
             router.refresh()
-            
+
             if (data?.status === 201) {
                 toast?.success('Question bookmarked successfully')
-            } else {
-                toast?.success('Question unbookmarked successfully')
             }
         },
         onError: (error) => {
+            setOptimisticState(isbookmarked) // Revert state on error
             toast?.error(error.message)
         }
     })
   }
 
   return (
-    <button onClick={handleBookmarkQuestion} disabled={isPending} className={cn('flex items-center cursor-pointer justify-center w-12 h-12 rounded-full',
+    <button onClick={handleBookmarkQuestion} className={cn('flex items-center cursor-pointer justify-center w-12 h-12 rounded-full',
         'bg-secondary/70 text-muted-foreground hover:bg-secondary/40 hover:text-white', {
-            'bg-amber-500/20 text-amber-500 hover:bg-amber-500/40 hover:text-white': isbookmarked,
+            'bg-amber-500/20 text-amber-500 hover:bg-amber-500/40 hover:text-white': optimisticState,
         })}>
-        {
-            isPending ? (<Loader2 className='animate-spin' size={18} />) : (
-                <LucideBookmark size={18} fill={
-                    isbookmarked ? 'currentColor' : 'none'
-                }  />
-            )
-        }
+
+        <LucideBookmark size={18} fill={
+            optimisticState ? 'currentColor' : 'none'
+        }  />
     </button>
   )
 }
