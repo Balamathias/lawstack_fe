d960c4e6e8cb69a37032361fe75bb7a9cf2cee68
Balamathias <balamathias05@gmail.com>
2025-03-02 11:51:31 +0100

    Base commit

--- src/components/home/globe-section.tsx
+++ src/components/home/globe-section.tsx
@@ -6,16 +6,16 @@ import Link from 'next/link'
 const GlobeSection = () => {
   return (
     <section className='py-20 flex flex-col items-center justify-center gap-y-5 bg-center bg-cover bg-no-repeat mt-10 h-full w-full relative max-w-4xl mx-auto'>
-        <Image src={'/global/globe.png'} width={500} height={500} alt='Globe' className='absolute' />
-        <h2 className='text-4xl sm:text-5xl font-bold py-3 text-center z-10'>
+        <Image src={'/global/globe.png'} width={500} height={500} alt='Globe' className='absolute hidden dark:flex' />
+        <h2 className='text-green-600 dark:text-foreground text-4xl sm:text-5xl font-bold py-3 text-center z-10'>
             Global Reach
         </h2>
         
-        <div className='bg-secondary/70 backdrop-blur p-6 rounded-xl flex flex-col gap-y-4 justify-between'>
+        <div className='bg-green-500/60 dark:bg-secondary/70 backdrop-blur p-6 rounded-xl flex flex-col gap-y-4 justify-between'>
             <h3 className='text-2xl font-semibold'>
                 Access Anywhere, Anytime
             </h3>
-            <p className='text-lg text-muted-foreground'>
+            <p className='text-lg text-black dark:text-muted-foreground'>
                 Our platform is accessible from anywhere in the world, with a global network of servers. Access your data and documents securely from any device, anywhere, anytime.
             </p>
 
--- src/components/home/neural-section.tsx
+++ src/components/home/neural-section.tsx
@@ -6,16 +6,16 @@ import Link from 'next/link'
 const NeuralSection = () => {
   return (
     <section className='py-20 flex flex-col items-center justify-center gap-y-5 bg-center bg-cover bg-no-repeat mt-10 h-full w-full relative max-w-4xl mx-auto'>
-        <Image src={'/global/neural.png'} width={333} height={333} quality={100} alt='Globe' className='absolute' />
-        <h2 className='text-4xl sm:text-5xl font-bold py-3 text-center z-10'>
+        <Image src={'/global/neural.png'} width={333} height={333} quality={100} alt='Globe' className='absolute hidden dark:flex' />
+        <h2 className='text-4xl sm:text-5xl font-bold py-3 text-center z-10 text-green-500 dark:text-foreground'>
             AI insights
         </h2>
         
-        <div className='bg-secondary/70 backdrop-blur p-6 rounded-xl flex flex-col gap-y-4 justify-between'>
+        <div className='bg-green-500/60 dark:bg-secondary/70 backdrop-blur p-6 rounded-xl flex flex-col gap-y-4 justify-between'>
             <h3 className='text-2xl font-semibold'>
                 Experience AI insights in Legal Research.
             </h3>
-            <p className='text-lg text-muted-foreground'>
+            <p className='text-lg text-black dark:text-muted-foreground'>
                 Our platform is powered by Artificial Intelligence, providing you with insights into your legal research. Get the most relevant information and insights to help you make better decisions.
             </p>
 
