34f9f9d39800658d63d2f14a1f4f1e6e21cd6434
Balamathias <balamathias05@gmail.com>
2025-03-16 18:09:01 +0100

    Hide nav

--- src/components/dashboard/navbar.tsx
+++ src/components/dashboard/navbar.tsx
@@ -30,7 +30,7 @@ const Navbar = ({ user }: NavbarProps) => {
     }
   })
 
-  if (pathname.match(/\/dashboard\/(questions|bookmarks|search|chat)\/[^/]+/)) {
+  if (pathname.match(/\/dashboard\/(past-questions|bookmarks|search|chat)\/[^/]+/)) {
     return null
   }
 
