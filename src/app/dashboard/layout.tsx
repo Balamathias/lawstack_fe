7c4c4d2302eb42919ea0fef5f540c35ba7cd3a14
Balamathias <balamathias05@gmail.com>
2025-03-19 21:48:00 +0100

    No auth

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -8,10 +8,6 @@ import Navbar from '@/components/dashboard/navbar'
 const Layout = async ({ children }: PropsWithChildren) => {
   const { data: user } = await getUser()
   
-  if (!user) {
-    return redirect('/login?next=/dashboard')
-  }
-  
   return (
     <div className='bg-background dark:bg-black min-h-screen'>
       <DashboardSidebar user={user} />
